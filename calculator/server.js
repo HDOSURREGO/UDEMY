aab0f7e7d26019cc4a5d8ba5dfe392ef65257cdc
Henry Hoyos <hdosurrego@gmail.com>
2020-05-13 00:05:14 -0400

    last to off

--- calculator/server.js
+++ calculator/server.js
@@ -6,10 +6,17 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + index.html);
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', function (req, res) {});
+app.post('/bmicalculator', function (req, res) {
+	const weight = req.body.weight;
+	const height = req.body.height;
+
+	const bmi = weight + height;
+
+	res.send('Your BMI is ' + bmi);
+});
 
 app.listen(3000, function (req, res) {
 	console.log('Server running on port 3000');
