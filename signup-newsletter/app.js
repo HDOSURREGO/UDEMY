8dd4ad8a9b3f775eaf973644bf5c9a68dc8cdbde
Henry Hoyos <hdosurrego@gmail.com>
2020-05-14 22:57:56 -0400

    port added

--- signup-newsletter/app.js
+++ signup-newsletter/app.js
@@ -53,6 +53,6 @@ app.post("/", function (req, res) {
 	request.end();
 });
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log("Server running on port 3000");
 });
